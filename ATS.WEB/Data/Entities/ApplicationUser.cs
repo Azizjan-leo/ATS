10fa145c563b1b7af505a289519fa9e1f9e922de
Azizjan Ayupov <litpulla@mail.ru>
2021-04-15 21:55:06 +0600

    ApplicationUser and Init migration

---
+++ ATS.WEB/Data/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ATS.WEB.Data.Entities {
+    public class ApplicationUser : IdentityUser {
+        [Required]
+        [StringLength(40, MinimumLength = 5)]
+        public virtual string Name { get; set; }
+    }
+}
