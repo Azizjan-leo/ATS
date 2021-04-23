a9e5d3ec1e19da97e7aa28be7aed88608e1cc398
Azizjan Ayupov <litpulla@mail.ru>
2021-04-23 06:53:06 +0600

    tempo

--- ATS.WEB/Startup.cs
+++ ATS.WEB/Startup.cs
@@ -70,7 +70,10 @@ namespace ATS.WEB
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
-            });
+                endpoints.MapControllerRoute(
+                  name: "default",
+                  pattern: "{controller=Home}/{action=Index}/{id?}");
+                });
 
             
         }
