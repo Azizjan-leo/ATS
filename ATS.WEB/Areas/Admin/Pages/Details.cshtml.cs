457f28296945e23e0072bfdb4200893a90e3bb08
Azizjan Ayupov <litpulla@mail.ru>
2021-05-05 07:57:32 +0600

    123

---
+++ ATS.WEB/Areas/Admin/Pages/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ATS.WEB.Data;
+using ATS.WEB.Data.Entities;
+
+namespace ATS.WEB.Areas.Admin.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ATS.WEB.Data.ApplicationDbContext _context;
+
+        public DetailsModel(ATS.WEB.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Cathedra Cathedra { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Cathedra = await _context.Cathedras.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Cathedra == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ ATS.WEB/Areas/Admin/Pages/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ATS.WEB.Data;
+using ATS.WEB.Data.Entities;
+
+namespace ATS.WEB.Areas.Admin.Pages
+{
+    public class EditModel : PageModel
+    {
+        private readonly ATS.WEB.Data.ApplicationDbContext _context;
+
+        public EditModel(ATS.WEB.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Cathedra Cathedra { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Cathedra = await _context.Cathedras.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Cathedra == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Cathedra).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CathedraExists(Cathedra.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool CathedraExists(int id)
+        {
+            return _context.Cathedras.Any(e => e.Id == id);
+        }
+    }
+}
