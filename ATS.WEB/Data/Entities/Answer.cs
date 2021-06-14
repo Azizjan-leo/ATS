ce21a03e81258d6bbd3b6b1c5b3ca890ee637467
Владимир Будалеев <inquisitive83@hotmail.com>
2021-06-14 22:48:19 +0500

    Testing

--- ATS.WEB/Data/Entities/Answer.cs
+++ ATS.WEB/Data/Entities/Answer.cs
@@ -7,9 +7,14 @@ namespace ATS.WEB.Data.Entities {
 
         [Display(Name = "Текст ответа")]
         public string AnswerText { get; set; }
+        public int? QuestionId { get; set; }
         [Display(Name = "Вопрос")]
         public virtual Question Question { get; set; }
         [Display(Name = "Правильный")]
         public bool IsRight { get; set; }
+        [Display(Name = "Ответ студента")]
+        public bool RightStudent { get; set; }
+        public int? TestResultQuestionId { get; set; }
+        public int? TestResultId { get; set; }
     }
 }
--- ATS.WEB/Data/Entities/TestResult.cs
+++ ATS.WEB/Data/Entities/TestResult.cs
@@ -1,18 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ATS.WEB.Data.Entities {
     [Index(nameof(AnswererId), nameof(TopicId), IsUnique = true)]
     public class TestResult {
         public int Id { get; set; }
         public int AnswererId { get; set; }
+        [Display(Name = "Студент")]
         public Student Answerer { get; set; }
         public int TopicId { get; set; }
+        [Display(Name = "Тема")]
         public Lesson Topic { get; set; }
+        [Display(Name = "Проверяющий")]
         public Teacher Reviewer { get; set; }
+        [Display(Name = "Дата сдачи")]
         public DateTime PassDate { get; set; }
         public List<Answer> Answers { get; set; }
+        [Display(Name = "Результат")]
         public int? Score { get; set; }
     }
 }
